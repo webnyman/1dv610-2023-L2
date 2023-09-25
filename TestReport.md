2fba73f00a2a5f90f626ed06891dfcf698ac48ad
webnyman <bjorn.nyman@webflex.se>
2023-09-25 16:29:47 +0200

    Update testreport.md

--- TestReport.md
+++ TestReport.md
@@ -7,4 +7,5 @@
 |Rovarsprak.translateToRovarSprak()|3|[länk till test](spec/support/Rovarsprak.spec.js)|
 |Rovarsprak.translateFromRovarSprak()|1|[länk till test](spec/support/Rovarsprak.spec.js)|
 |Rovarsprak.validateTextInput()|2|[länk till test](spec/support/Rovarsprak.spec.js)|
-
+|Rot13.encodeToROT13()|2|[länk till test](spec/support/Rot13.spec.js)|
+|Rot13.decodeFromROT13()|2|[länk till test](spec/support/Rot13.spec.js)|
--- spec/support/Rot13.spec.js
+++ spec/support/Rot13.spec.js
@@ -1,6 +1,6 @@
-// Tests for the Rot13 class
 import { Rot13 } from '../../Rot13.js'
 
+// Tests for the Rot13 class
 describe('Rot13', () => {
   let rot13
 
@@ -8,19 +8,21 @@ describe('Rot13', () => {
     rot13 = new Rot13()
   })
 
+  // Test cases (2) for encodeToROT13
   it('should encode text to ROT13', () => {
     const encodedText = rot13.encodeToROT13('Hello World')
     expect(encodedText).toBe('Uryyä Gäbyq')
   })
 
-  it('should decode text from ROT13', () => {
-    const decodedText = rot13.decodeFromROT13('Uryyä Gäbyq')
-    expect(decodedText).toBe('Hello World')
-  })
-
   it('should handle an empty string when encoding', () => {
     const encodedText = rot13.encodeToROT13('')
     expect(encodedText).toBe('Texten är tom')
+  })
+
+  // Test cases (2) for decodeFromROT13
+  it('should decode text from ROT13', () => {
+    const decodedText = rot13.decodeFromROT13('Uryyä Gäbyq')
+    expect(decodedText).toBe('Hello World')
   })
 
   it('should handle an empty string when decoding', () => {
