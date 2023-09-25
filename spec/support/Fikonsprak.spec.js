0ff8a89929f9d6a6028b51f0e08c8c31a9d630e4
webnyman <bjorn.nyman@webflex.se>
2023-09-25 16:13:27 +0200

    Add testresult.md

--- spec/support/Fikonsprak.spec.js
+++ spec/support/Fikonsprak.spec.js
@@ -1,20 +1,16 @@
 import { Fikonsprak } from '../../Fikonsprak.js'
 
+// Tests for the Fikonsprak class
 describe('Fikonsprak', () => {
   let fikonsprak
 
   beforeEach(() => {
     fikonsprak = new Fikonsprak()
   })
-
+  // Test cases (3) for translateToFikonSprak
   it('should translate text to Fikonsprak', () => {
     const translatedText = fikonsprak.translateToFikonSprak('Kaffe')
     expect(translatedText).toBe('fiffe kakon')
-  })
-
-  it('should translate text from Fikonsprak', () => {
-    const decodedText = fikonsprak.translateFromFikonSprak('fiffe kakon')
-    expect(decodedText).toBe('kaffe')
   })
 
   it('should handle an empty input when translating to Fikonsprak', () => {
@@ -22,16 +18,23 @@ describe('Fikonsprak', () => {
     expect(emptyInput).toBe('Texten är tom')
   })
 
-  it('should handle input with no vowels when translating to Fikonsprak', () => {
-    const noVowelsInput = fikonsprak.translateToFikonSprak('kffr')
-    expect(noVowelsInput).toBe('Texten innehåller inga vokaler')
-  })
-
   it('should handle input with more than one word when translating to Fikonsprak', () => {
     const multipleWordsInput = fikonsprak.translateToFikonSprak('Kaffe är gott')
     expect(multipleWordsInput).toBe('Det går bara att översätta ett ord i taget')
   })
 
+  // Test cases (2) for translateFromFikonSprak
+  it('should translate text from Fikonsprak', () => {
+    const decodedText = fikonsprak.translateFromFikonSprak('fiffe kakon')
+    expect(decodedText).toBe('kaffe')
+  })
+
+  it('should handle input with no vowels when translating to Fikonsprak', () => {
+    const noVowelsInput = fikonsprak.translateToFikonSprak('kffr')
+    expect(noVowelsInput).toBe('Texten innehåller inga vokaler')
+  })
+
+  // Test cases (2) for isFikonSprak
   it('should check if text is in Fikonsprak', () => {
     const fikonText = 'fiffe kakon'
     const nonFikonText = 'fiffa kakan'
