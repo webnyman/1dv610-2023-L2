59b761dd3a7f0f6de5bb622a27a2c1b654d0d28d
webnyman <bjorn.nyman@webflex.se>
2023-09-25 07:57:49 +0200

    Add reflections.md

---
+++ Reflections.md
@@ -0,0 +1,4 @@
+# Reflektioner kapitel 2
+|Namn och förklaring|Reflektion och regler från Clean Code|
+|-------------------|-------------------------------------|
+|** Fikonsprak namn på huvudklassen|
--- spec/support/Fikonsprak.spec.js
+++ spec/support/Fikonsprak.spec.js
@@ -1,43 +1,42 @@
-// Import the Fikonsprak class
-import { Fikonsprak } from '../../Fikonsprak.js' // Adjust the path as needed
+import { Fikonsprak } from '../../Fikonsprak.js'
 
 describe('Fikonsprak', () => {
-  let fikonsprak;
+  let fikonsprak
 
   beforeEach(() => {
-    fikonsprak = new Fikonsprak();
-  });
+    fikonsprak = new Fikonsprak()
+  })
 
   it('should translate text to Fikonsprak', () => {
-    const translatedText = fikonsprak.translateToFikonSprak('Kaffe');
-    expect(translatedText).toBe('fiffe kakon');
-  });
+    const translatedText = fikonsprak.translateToFikonSprak('Kaffe')
+    expect(translatedText).toBe('fiffe kakon')
+  })
 
   it('should translate text from Fikonsprak', () => {
-    const decodedText = fikonsprak.translateFromFikonSprak('fiffe kakon');
-    expect(decodedText).toBe('kaffe');
-  });
+    const decodedText = fikonsprak.translateFromFikonSprak('fiffe kakon')
+    expect(decodedText).toBe('kaffe')
+  })
 
   it('should handle an empty input when translating to Fikonsprak', () => {
-    const emptyInput = fikonsprak.translateToFikonSprak('');
-    expect(emptyInput).toBe('Texten är tom');
-  });
+    const emptyInput = fikonsprak.translateToFikonSprak('')
+    expect(emptyInput).toBe('Texten är tom')
+  })
 
   it('should handle input with no vowels when translating to Fikonsprak', () => {
-    const noVowelsInput = fikonsprak.translateToFikonSprak('kffr');
-    expect(noVowelsInput).toBe('Texten innehåller inga vokaler');
-  });
+    const noVowelsInput = fikonsprak.translateToFikonSprak('kffr')
+    expect(noVowelsInput).toBe('Texten innehåller inga vokaler')
+  })
 
   it('should handle input with more than one word when translating to Fikonsprak', () => {
-    const multipleWordsInput = fikonsprak.translateToFikonSprak('Kaffe är gott');
-    expect(multipleWordsInput).toBe('Det går bara att översätta ett ord i taget');
-  });
+    const multipleWordsInput = fikonsprak.translateToFikonSprak('Kaffe är gott')
+    expect(multipleWordsInput).toBe('Det går bara att översätta ett ord i taget')
+  })
 
   it('should check if text is in Fikonsprak', () => {
-    const fikonText = 'fiffe kakon';
-    const nonFikonText = 'fiffa kakan';
+    const fikonText = 'fiffe kakon'
+    const nonFikonText = 'fiffa kakan'
 
-    expect(fikonsprak.isFikonSprak(fikonText)).toBe(true);
-    expect(fikonsprak.isFikonSprak(nonFikonText)).toBe(false);
-  });
-});
+    expect(fikonsprak.isFikonSprak(fikonText)).toBe(true)
+    expect(fikonsprak.isFikonSprak(nonFikonText)).toBe(false)
+  })
+})
