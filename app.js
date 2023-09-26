686426bc5fb8959ae97df7b1f115a3ae93de5932
webnyman <bjorn.nyman@webflex.se>
2023-09-26 09:25:40 +0200

    Add FACADE pattern to module

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-import { RovarSprak } from './Rovarsprak.js'
-import { Rot13 } from './Rot13.js'
-import { Fikonsprak } from './Fikonsprak.js'
+import { cypher } from "./cypher.js"
 
-const rovarSprakLibrary = new RovarSprak()
-const rot13Library = new Rot13()
-const fikonSprakLibrary = new Fikonsprak()
+const textToCypher = "Hello"
 
-const text = 'Hej på dig!'
-const text2 = 'Kaffe'
-const text3 = 'fiffe kakon'
-console.log(rovarSprakLibrary.translateToRovarSprak(text))
-console.log(rovarSprakLibrary.translateFromRovarsprak(rovarSprakLibrary.translateToRovarSprak(text)))
-console.log(rot13Library.encodeToROT13(text))
-console.log(rot13Library.decodeFromROT13(rot13Library.encodeToROT13(text)))
-console.log(fikonSprakLibrary.translateToFikonSprak(text2))
-console.log(fikonSprakLibrary.translateFromFikonSprak(text3))
+const fikonsprak = cypher.fikonsprak.to(textToCypher)
+const rot13 = cypher.rot13.to(textToCypher)
+const rovarsprak = cypher.rovarsprak.to(textToCypher)
+
+const decodedFikonsprak = cypher.fikonsprak.from(fikonsprak)
+const decodedRot13 = cypher.rot13.from(rot13)
+const decodedRovarsprak = cypher.rovarsprak.from(rovarsprak)
+
+console.log(fikonsprak)
+console.log(decodedFikonsprak)
+console.log(rot13)
+console.log(decodedRot13)
+console.log(rovarsprak)
+console.log(decodedRovarsprak)
---
+++ cypher.js
@@ -0,0 +1,24 @@
+import { Fikonsprak } from './src/Fikonsprak.js'
+import { Rot13 } from './src/Rot13.js'
+import { RovarSprak } from './src/Rovarsprak.js'
+
+const rovarSprakLibrary = new RovarSprak()
+const rot13Library = new Rot13()
+const fikonSprakLibrary = new Fikonsprak()
+
+const cypher = {
+    fikonsprak: {
+        to: (text) => fikonSprakLibrary.translateToFikonSprak(text),
+        from: (text) => fikonSprakLibrary.translateFromFikonSprak(text)
+    },
+    rot13: {
+        to: (text) => rot13Library.encodeToROT13(text),
+        from: (text) => rot13Library.decodeFromROT13(text)
+    },
+    rovarsprak: {
+        to: (text) => rovarSprakLibrary.translateToRovarSprak(text),
+        from: (text) => rovarSprakLibrary.translateFromRovarsprak(text)
+    }
+}
+
+export { cypher }
